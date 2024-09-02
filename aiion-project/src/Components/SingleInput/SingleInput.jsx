06636a6a9b85e34c887ed4c17e91fdd3c7b697c7
aliayman12 <aliaymann12@gmail.com>
2024-09-02 19:07:34 +0300

    single input section updated 2

--- aiion-project/src/Components/SingleInput/SingleInput.jsx
+++ aiion-project/src/Components/SingleInput/SingleInput.jsx
@@ -47,53 +47,11 @@ function SingleInput() {
     <>
       <div className="container container-form my-5 rounded p-3">
         <div className="row">
-          <div className="col-12 col-md-3 p-4 text-center text-md-start welcome-section">
-            <h3 className="mb-3">Explanation</h3>
-
-            <table className="table table-striped table-bordered table-hover table-sm table-responsive" style={tableStyle}>
-            <thead>
-              <tr>
-                <th>classical geothermometers</th>
-                <th className="align-middle">t(C)</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>temp</td>
-                <td>{sum !== null ? <span>{sum}</span> : "N/A"}</td>
-              </tr>
-              <tr>
-                <td>quartz (four&pott82)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>am.silica (four77)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>chalcedony (four77)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>na-k-ca (four&true73)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>na-k (gigg88)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>k-mg (gigg88)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-              <tr>
-                <td>na-li (foui&mich81)</td>
-                <td className="align-middle">N/A</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="col-md-2">
+            <h3>Explanation</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nesciunt. Porro, fuga tenetur modi voluptates id maiores repudiandae doloremque quibusdam.</p>
           </div>
-          <div className="col-12 col-md-9 form-section text-dark p-4">
+          <div className="col-md-7 form-section text-dark p-4">
             <h3 className="mb-4">Free Trial</h3>
             <form onSubmit={formik.handleSubmit}>
               <div className="row">
@@ -197,6 +155,56 @@ function SingleInput() {
               </div>
             </form>
           </div>
+          <div className="col-md-3 text-center text-md-start welcome-section">
+            <h3 className="mb-3">Result</h3>
+
+            <table
+              className="table table-striped table-bordered table-hover table-sm table-responsive"
+              style={tableStyle}
+            >
+              <thead>
+                <tr>
+                  <th>classical geothermometers</th>
+                  <th className="align-middle">t(C)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>temp</td>
+                  <td>{sum !== null ? <span>{sum}</span> : "N/A"}</td>
+                </tr>
+                <tr>
+                  <td>quartz (four&pott82)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>am.silica (four77)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>chalcedony (four77)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>na-k-ca (four&true73)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>na-k (gigg88)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>k-mg (gigg88)</td>
+                  <td>N/A</td>
+                </tr>
+                <tr>
+                  <td>na-li (foui&mich81)</td>
+                  <td className="align-middle">N/A</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
         </div>
       </div>
     </>
