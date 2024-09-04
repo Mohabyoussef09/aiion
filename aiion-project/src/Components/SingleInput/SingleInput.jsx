e3ac7bbc6067cba0d8d91195013a33383bd1c5e5
aliayman12 <aliaymann12@gmail.com>
2024-09-05 00:15:23 +0300

    single inputs loading

--- aiion-project/src/Components/SingleInput/SingleInput.jsx
+++ aiion-project/src/Components/SingleInput/SingleInput.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
 import axios from "axios";
+import Loading from "../Loading/Loading";
 
 function SingleInput() {
-  const [sum, setSum] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleSingleInputs(values) {
-    console.log(values);
+    setLoading(true);
 
     axios
       .post("http://localhost:3000/sum", values)
       .then((res) => {
-        console.log(res.data);
-        setSum(res.data.sum);
+        setTimeout(() => {
+          setResponse(res);
+          setLoading(false); 
+        }, 2000); 
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   }
 
@@ -36,12 +41,8 @@ function SingleInput() {
 
   function handleClear() {
     formik.resetForm();
-    setSum(null);
+    setResponse(null);
   }
-
-  const tableStyle = {
-    fontFamily: `'IBM Plex Mono', monospace`,
-  };
 
   return (
     <>
@@ -49,7 +50,11 @@ function SingleInput() {
         <div className="row">
           <div className="col-md-2">
             <h3>Explanation</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, nesciunt. Porro, fuga tenetur modi voluptates id maiores repudiandae doloremque quibusdam.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+              nesciunt. Porro, fuga tenetur modi voluptates id maiores
+              repudiandae doloremque quibusdam.
+            </p>
           </div>
           <div className="col-md-7 form-section text-dark p-4">
             <h3 className="mb-4">Free Trial</h3>
@@ -156,55 +161,59 @@ function SingleInput() {
             </form>
           </div>
           <div className="col-md-3 text-center text-md-start welcome-section">
-            <h3 className="mb-3">Result</h3>
-
-            <table
-              className="table table-striped table-bordered table-hover table-sm table-responsive"
-              style={tableStyle}
-            >
-              <thead>
-                <tr>
-                  <th>classical geothermometers</th>
-                  <th className="align-middle">t(C)</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>temp</td>
-                  <td>{sum !== null ? <span>{sum}</span> : "N/A"}</td>
-                </tr>
-                <tr>
-                  <td>quartz (four&pott82)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>am.silica (four77)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>chalcedony (four77)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>na-k-ca (four&true73)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>na-k (gigg88)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>k-mg (gigg88)</td>
-                  <td>N/A</td>
-                </tr>
-                <tr>
-                  <td>na-li (foui&mich81)</td>
-                  <td className="align-middle">N/A</td>
-                </tr>
-              </tbody>
-            </table>
+            <h3 className="mb-3 text-center">Results</h3>
+            <div className="d-flex justify-content-center align-items-center h-75">
+              {loading ? (
+                <Loading />
+              ) : (
+                <table
+                  className="table table-striped table-bordered table-hover table-sm table-responsive"
+                  style={{display: response !== null ? 'block' : 'none'}} 
+                >
+                  <thead>
+                    <tr>
+                      <th>Classical Geothermometers</th>
+                      <th className="align-middle">T(C)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Temp</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>Quartz (four&pott82)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>am.Silica (four77)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>Chalcedony (four77)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>Na-K-Ca (four&true73)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>Na-K (gigg88)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>K-Mg (gigg88)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                    <tr>
+                      <td>Na-Li (foui&mich81)</td>
+                      <td>{response !== null ? <span>{response.data.sum}</span> : "N/A"}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              )}
+            </div>
           </div>
-
         </div>
       </div>
     </>
@@ -212,3 +221,300 @@ function SingleInput() {
 }
 
 export default SingleInput;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import { useFormik } from "formik";
+// import axios from "axios";
+// import Loading from "../Loading/Loading";
+
+// function SingleInput() {
+//   const [response, setResponse] = useState(null);
+//   const [loading, setLoading] = useState(false);
+
+//   function handleSingleInputs(values) {
+//     setLoading(true);
+
+//     axios
+//       .post("http://localhost:3000/sum", values)
+//       .then((res) => {
+//         setTimeout(() => {
+//           console.log(res);
+
+//           setResponse(res.data.sum);
+//           setLoading(false);
+//         }, 2000);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         setLoading(false);
+//       });
+//   }
+
+//   const formik = useFormik({
+//     initialValues: {
+//       ph: "",
+//       ec: "",
+//       k: "",
+//       na: "",
+//       mg: "",
+//       ca: "",
+//       cl: "",
+//       sio2: "",
+//       f: "",
+//     },
+//     onSubmit: handleSingleInputs,
+//   });
+
+//   function handleClear() {
+//     formik.resetForm();
+//     setResponse(null);
+//   }
+
+//   return (
+//     <>
+//       <div className="container container-form my-5 rounded p-3">
+//         <div className="row">
+//           <div className="col-md-2">
+//             <h3>Explanation</h3>
+//             <p>
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+//               nesciunt. Porro, fuga tenetur modi voluptates id maiores
+//               repudiandae doloremque quibusdam.
+//             </p>
+//           </div>
+//           <div className="col-md-7 form-section text-dark p-4">
+//             <h3 className="mb-4">Free Trial</h3>
+//             <form onSubmit={formik.handleSubmit}>
+//               <div className="row">
+//                 <div className="col-12 col-md-6 mb-3">
+//                   <label htmlFor="ph">ph</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="ph"
+//                     name="ph"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.ph}
+//                   />
+
+//                   <label htmlFor="ec">ec</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="ec"
+//                     name="ec"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.ec}
+//                   />
+
+//                   <label htmlFor="k">k</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="k"
+//                     name="k"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.k}
+//                   />
+
+//                   <label htmlFor="na">na</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="na"
+//                     name="na"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.na}
+//                   />
+
+//                   <label htmlFor="mg">mg</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="mg"
+//                     name="mg"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.mg}
+//                   />
+//                 </div>
+//                 <div className="col-12 col-md-6 mb-3">
+//                   <label htmlFor="ca">ca</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="ca"
+//                     name="ca"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.ca}
+//                   />
+
+//                   <label htmlFor="cl">cl</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="cl"
+//                     name="cl"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.cl}
+//                   />
+
+//                   <label htmlFor="sio2">sio2</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="sio2"
+//                     name="sio2"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.sio2}
+//                   />
+
+//                   <label htmlFor="f">f</label>
+//                   <input
+//                     type="text"
+//                     className="form-control mb-3"
+//                     id="f"
+//                     name="f"
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values.f}
+//                   />
+//                 </div>
+//               </div>
+//               <div className="flex d-flex justify-content-center">
+//                 <button type="submit" className="btn btn-outline-primary">
+//                   Predict
+//                 </button>
+//                 <button
+//                   type="button"
+//                   className="btn btn-outline-danger mx-2"
+//                   onClick={handleClear}
+//                 >
+//                   Clear
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+
+//           <div className="col-md-3 text-center text-md-start welcome-section">
+//             <h3 className="mb-3 text-center">Results</h3>
+//             <div className="d-flex justify-content-center align-items-center h-75">
+//               {loading ? (
+//                 <Loading />
+//               ) : (
+//                 <table
+//                   className="table table-striped table-bordered table-hover table-sm table-responsive"
+//                   style={{ display: response !== null ? "block" : "none" }}
+//                 >
+//                   <thead>
+//                     <tr>
+//                       <th>Classical Geothermometers</th>
+//                       <th className="align-middle">T(C)</th>
+//                     </tr>
+//                   </thead>
+//                   <tbody>
+//                     <tr>
+//                       <td>Temp</td>
+//                       <td>
+//                         {response !== null ? <span>{response}</span> : "N/A"}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>Quartz (four&pott82)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.quartz}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>am.Silica (four77)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.silica}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>Chalcedony (four77)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.chalcedony}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>Na-K-Ca (four&true73)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.na - k - ca}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>Na-K (gigg88)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.na - k}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>K-Mg (gigg88)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.k - mg}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                     <tr>
+//                       <td>Na-Li (foui&mich81)</td>
+//                       <td>
+//                         {response !== null ? (
+//                           <span>{response.na - li}</span>
+//                         ) : (
+//                           "N/A"
+//                         )}
+//                       </td>
+//                     </tr>
+//                   </tbody>
+//                 </table>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SingleInput;
+
+
